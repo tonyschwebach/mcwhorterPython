58b4482dd28986a82300806f2b3388cdde6c8fc8
T Schway <tony.schwebach@gmail.com>
2021-05-25 09:09:10 -0400

    simple sort

--- forLoop.py
+++ forLoop.py
@@ -8,7 +8,19 @@ for i in range(0,numGrades,1):
   grades.append(grade)
   total += grade
 # then print each grade
-print('Your average is: '+ str(total/len(grades)))
+print('Your average is: ', total/len(grades))
+print('Your high score is: ', max(grades))
+print('Your low score is: ', min(grades))
+
+for i in range (0, numGrades-1,1):
+  for i in range (0, numGrades-1,1):
+    if grades[i]<grades[i+1]:
+      temp = grades[i]
+      grades[i]=grades[i+1]
+      grades[i+1]=temp
+
+
 for grade in grades:
   print(grade)
+  
 
